dbb3ccd591b10023f45ff52266537788f38e0fe5
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-30 23:57:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@
   original_array.each do|sentence|
     return_array << sentence.reverse
   end
-  return_array.join(" ")
+  
   
 end
 
@@ -15,7 +15,6 @@ def reverse_each_word(sentence)
   array.collect do|string| 
     test_array << sentence.reverse
   end
-  test_array.join(" ")
 end
 
 

edda44be5ca0cc484513cb75e5faeb5068dfb492
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-30 23:55:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,21 @@
  def reverse_each_word(sentence)
-  sentence.each do |word|
-    puts "Hello there, and how are you?"
+  original_array = sentence.split(" ")
+  return_array = []
+  
+  original_array.each do|sentence|
+    return_array << sentence.reverse
   end
+  return_array.join(" ")
 end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ") 
+  test_array = []
+  array.collect do|string| 
+    test_array << sentence.reverse
+  end
+  test_array.join(" ")
+end
+
+
+reverse_each_word("Hello there, and how are you?")
